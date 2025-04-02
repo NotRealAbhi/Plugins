3b00c281d0e46d4ff86b3f5289850d4c65bb74d9
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-03 02:03:12 +0530

    Create __init__.py

---
+++ Abhi/__init__.py
@@ -0,0 +1,21 @@
+"""
+Telegram @Itz_Your_4Bhi
+Copyright ©️ 2025
+"""
+
+import asyncio
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+from pytgcalls.types import GroupCallParticipant
+
+from Player.Core.Bot import MusicBot, MusicUser
+from .Logging import LOGGER
+from Abhi.Misc import sudo
+
+sudo()
+
+app = MusicBot
+call = MusicUser
+
+
+seek_chats = {}
