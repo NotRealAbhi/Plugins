7fd776171ed685847dd77de681ebb8570ac36872
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-03 11:01:23 +0530

    Create Config.py

---
+++ Config/Config.py
@@ -0,0 +1,28 @@
+"""
+Telegram @Itz_Your_4Bhi
+Copyright ©️ 2025
+"""
+
+
+import os
+
+
+API_ID: int = int(os.getenv("API_ID", 7249983))
+
+API_HASH: str = os.getenv("API_HASH", "be8ea36c220d5e879c91ad9731686642")
+
+SESSION_STRING: str = os.getenv("SESSION_STRING", "BQBuoD8AaToj-og3EmiucjJSDY5NdMvMlYSqHvd5i9KZDKQDopHDiRF7kMH_fJoc-_vl1VzofOYkmfTdNbuIqSPLSMA9kTBw70i0_dF3mwijWYKwrwbVbyvo9H97CLHS7Rvi0rBKJALcLEkH8WrE86qzstTjAlmQxxL82lu5giU62IyAlFgYXT1UFe-2PAu95Y8uN9klwBb59bQBBnMjJnxs4ksV7AXYK-zCx5-GzfVNMpDdHWE574syxUoD5DW9icTfLWSX8Ahco2QQYEW9f5EAp4mdBOa7OUmY2YpiRFHMbP9_DCGReJbPyX-UB1Ary6mv1uo-cxaRDeVWP3ltjjy-hVfTlAAAAAHgdgpVAA")
+
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 7862043458))]
+
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", --1002369363228))
+
+PREFIX: str = str("/")
+
+RPREFIX: str = str("-")
+
+
+
+# No Need To Edit Below This
+
+LOG_FILE_NAME: str = "Abhi.txt"
