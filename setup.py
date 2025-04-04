eb9ce64a95fa4d75b7444edad2b6914241bdd220
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-05 02:44:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 from setuptools import setup, find_packages
 
-def read_requirements():
-    with open("requirements.txt", "r") as f:
-        return f.read().splitlines()
-
-
 setup(
     name="AbhiMusicPlug",
-    version="1.0",
-    packages=find_packages(),
-    install_requires=read_requirements(),
-    author="Abhi",
+    version="1.0.0",
+    author="AbhiShek",
     author_email="abhishekbanshiwal2005@gmail.com",
-    description="A plugin package for reuse",
+    description="A Telegram Music Plugin System",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/NotRealAbhi/Plugins",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
 )
