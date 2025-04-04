e36d4c6dd38bfc684fea0f870c0568b6e918f898
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-04 23:49:59 +0530

    Update Test.py

--- Abhi/Test/Test.py
+++ Abhi/Test/Test.py
@@ -1,9 +1,2 @@
-import unittest
-from Abhi import greet
-
-class TestExamplePlugin(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual(greet("Abhi"), "Hello, Abhi! This is a plugin.")
-
-if __name__ == '__main__':
-    unittest.main()
+def greet():
+    return "Hello from Abhi!"
