1346c0aed342f469f3aa3c29f652beada1693ee8
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-05 01:23:05 +0530

    Update Bot.py

--- Abhi/Core/Bot.py
+++ Abhi/Core/Bot.py
@@ -7,12 +7,12 @@ Copyright ©️ 2025
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-import config
+import Config
 from ..Logging import LOGGER
 
-api_id: int = config.API_ID
-api_hash: str = config.API_HASH
-session_string: str = config.SESSION_STRING
+api_id: int = Config.API_ID
+api_hash: str = Config.API_HASH
+session_string: str = Config.SESSION_STRING
 
 MusicBot = Client(
     name="Abhi", api_id=api_id, api_hash=api_hash, session_string=session_string
