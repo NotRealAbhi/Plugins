5a4252f88a7ff45a05dee43065cda7ecae76dd4f
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-04 23:35:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AbhiMusicPlug",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Abhi",
+    author_email="abhishekbanshiwal2005@gmail.com",
+    description="A plugin package for reuse",
+    url="https://github.com/NotRealAbhi/Plugins",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
